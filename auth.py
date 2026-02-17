771d4e2379c52c59dc82773168c6350f428e7b22
mumbimuthiga <veronicmuthiga@gmail.com>
2026-02-17 22:05:49 +0300

    signup and login using jwt and mysql

---
+++ auth.py
@@ -0,0 +1,16 @@
+from passlib.context import CryptContext
+from jose import jwt
+
+SECRET_KEY = "secretkey"
+ALGORITHM = "HS256"
+
+pwd_context = CryptContext(schemes=["bcrypt"])
+
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+def verify_password(plain, hashed):
+    return pwd_context.verify(plain, hashed)
+
+def create_token(data: dict):
+    return jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
--- main.py
+++ main.py
@@ -1,37 +1,58 @@
-from fastapi import FastAPI ,HTTPException;
-from pydantic import BaseModel
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
 
-app=FastAPI()
+import models, schemas, auth
+from database import engine, SessionLocal
 
-class Item(BaseModel):
-    text:str
-    isdone:bool=False
+models.Base.metadata.create_all(bind=engine)
 
-
-items=[]
-
-@app.get('/')
-def root():
-    return {"Hello" :"World"}
+app = FastAPI()
 
 
-@app.post('/items')
-def create_item(Item:Item):
-    items.append(Item)
-    return items
-
-@app.get('/items',response_model=list[Item])
-def list_items(limit : int=10):
-    return items[0:limit]
-
-
-@app.get('/items/{item_id}',response_model=Item)
-def get_item(item_id:int)->Item :
-    # print(len(items))
-    if item_id < len(items):
-        return items[item_id]
-    else:
-        raise HTTPException(status_code=404,detail=f"Item {item_id} not Found")
-  
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
+# REGISTER
+@app.post("/register")
+def register(user: schemas.UserCreate, db: Session = Depends(get_db)):
+
+    hashed_pw = auth.hash_password(user.password)
+
+    db_user = models.User(
+        email=user.email,
+        hashed_password=hashed_pw
+    )
+
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+
+    return {"message": "User registered"}
+
+
+# LOGIN
+@app.post("/login")
+def login(user: schemas.UserLogin, db: Session = Depends(get_db)):
+
+    db_user = db.query(models.User).filter(
+        models.User.email == user.email
+    ).first()
+
+    if not db_user:
+        raise HTTPException(status_code=400, detail="User not found")
+
+    if not auth.verify_password(user.password, db_user.hashed_password):
+        raise HTTPException(status_code=400, detail="Wrong password")
+
+    token = auth.create_token({"sub": db_user.email})
+
+    return {
+        "access_token": token,
+        "token_type": "bearer"
+    }
